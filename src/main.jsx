f263a011c1548a4c1493d50fa113c17b820929da
Arun6381 <kumararun28092@gmail.com>
2024-04-27 18:43:32 +0530

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// index.js or App.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Import your Tailwind CSS styles
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
